2356200fff798168b73719f1bed93e928f1f1747
Yuichi Motoyama <yomichi@tsg.jp>
2016-10-12 23:08:51 +0900

    change indent width 2=>4

--- src/lattice.jl
+++ src/lattice.jl
@@ -1,11 +1,11 @@
 type Lattice
-  dim :: Int
-  size :: Vector{Int}
-  nsites :: Int
-  nbonds :: Int
-  neighbors :: Matrix{Int}
-  source :: Vector{Int}
-  target :: Vector{Int}
+    dim :: Int
+    size :: Vector{Int}
+    nsites :: Int
+    nbonds :: Int
+    neighbors :: Matrix{Int}
+    source :: Vector{Int}
+    target :: Vector{Int}
 end
 
 import Base.size
@@ -67,16 +67,16 @@ target(lat::Lattice, bond::Integer) = lat.target[bond]
 generate chain lattice with length `L`.
 """
 function chain_lattice(L::Integer)
-  neighbors = zeros(Int,2,L)
-  source = zeros(Int,L)
-  target = zeros(Int,L)
-  for s in 1:L
-    neighbors[1,s] = mod1(s+1,L)
-    neighbors[2,s] = mod1(s-1,L)
-    source[s] = s
-    target[s] = neighbors[1,s]
-  end
-  Lattice(1,[L],L,L,neighbors,source,target)
+    neighbors = zeros(Int,2,L)
+    source = zeros(Int,L)
+    target = zeros(Int,L)
+    for s in 1:L
+        neighbors[1,s] = mod1(s+1,L)
+        neighbors[2,s] = mod1(s-1,L)
+        source[s] = s
+        target[s] = neighbors[1,s]
+    end
+    Lattice(1,[L],L,L,neighbors,source,target)
 end
 
 """
@@ -86,24 +86,24 @@ generate square lattice with size `L` \\times `W`.
 """
 square_lattice(L::Integer) = square_lattice(L,L)
 function square_lattice(L::Integer, W::Integer)
-  s2xy(s::Integer) = mod(s-1,L),div(s-1,L)
-  xy2s(x::Integer, y::Integer) = mod(y,W)*L + mod(x,L) + 1
+    s2xy(s::Integer) = mod(s-1,L),div(s-1,L)
+    xy2s(x::Integer, y::Integer) = mod(y,W)*L + mod(x,L) + 1
 
-  nsites = L*W
-  nbonds = 2nsites
-  neighbors = zeros(Int,4,nsites)
-  source = zeros(Int,nbonds)
-  target = zeros(Int,nbonds)
-  for s in 1:nsites
-    x,y = s2xy(s)
-    neighbors[1,s] = xy2s(x+1,y)
-    neighbors[2,s] = xy2s(x,y+1)
-    neighbors[3,s] = xy2s(x-1,y)
-    neighbors[4,s] = xy2s(x,y-1)
-    source[2s-1:2s] = s
-    target[2s-1:2s] = neighbors[1:2,s]
-  end
-  Lattice(2,[L,W],nsites,nbonds,neighbors,source,target)
+    nsites = L*W
+    nbonds = 2nsites
+    neighbors = zeros(Int,4,nsites)
+    source = zeros(Int,nbonds)
+    target = zeros(Int,nbonds)
+    for s in 1:nsites
+        x,y = s2xy(s)
+        neighbors[1,s] = xy2s(x+1,y)
+        neighbors[2,s] = xy2s(x,y+1)
+        neighbors[3,s] = xy2s(x-1,y)
+        neighbors[4,s] = xy2s(x,y-1)
+        source[2s-1:2s] = s
+        target[2s-1:2s] = neighbors[1:2,s]
+    end
+    Lattice(2,[L,W],nsites,nbonds,neighbors,source,target)
 end
 
 """
@@ -113,23 +113,23 @@ generate triangular lattice with size `L` \\times `W`.
 """
 triangular_lattice(L::Integer) = triangular_lattice(L,L)
 function triangular_lattice(L::Integer, W::Integer)
-  s2xy(s::Integer) = mod(s-1,L),div(s-1,L)
-  xy2s(x::Integer, y::Integer) = mod(y,W)*L + mod(x,L) + 1
-  nsites = L*W
-  nbonds = 3nsites
-  neighbors = zeros(Int,6,nsites)
-  source = zeros(Int,nbonds)
-  target = zeros(Int,nbonds)
-  for s in 1:nsites
-    x,y = s2xy(s)
-    neighbors[1,s] = xy2s(x+1,y)
-    neighbors[2,s] = xy2s(x,y+1)
-    neighbors[3,s] = xy2s(x+1,y+1)
-    neighbors[4,s] = xy2s(x-1,y)
-    neighbors[5,s] = xy2s(x,y-1)
-    neighbors[6,s] = xy2s(x-1,y-1)
-    source[3s-2:3s] = s
-    target[3s-2:3s] = neighbors[1:3,s]
-  end
-  Lattice(2,[L,W],nsites,nbonds,neighbors,source,target)
+    s2xy(s::Integer) = mod(s-1,L),div(s-1,L)
+    xy2s(x::Integer, y::Integer) = mod(y,W)*L + mod(x,L) + 1
+    nsites = L*W
+    nbonds = 3nsites
+    neighbors = zeros(Int,6,nsites)
+    source = zeros(Int,nbonds)
+    target = zeros(Int,nbonds)
+    for s in 1:nsites
+        x,y = s2xy(s)
+        neighbors[1,s] = xy2s(x+1,y)
+        neighbors[2,s] = xy2s(x,y+1)
+        neighbors[3,s] = xy2s(x+1,y+1)
+        neighbors[4,s] = xy2s(x-1,y)
+        neighbors[5,s] = xy2s(x,y-1)
+        neighbors[6,s] = xy2s(x-1,y-1)
+        source[3s-2:3s] = s
+        target[3s-2:3s] = neighbors[1:3,s]
+    end
+    Lattice(2,[L,W],nsites,nbonds,neighbors,source,target)
 end
