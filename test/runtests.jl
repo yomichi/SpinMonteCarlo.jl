ebff2d1a5164efece99de5f5f21e976f7f752307
Yuichi Motoyama <yomichi@tsg.jp>
2018-04-06 23:32:11 +0900

    rm anisotropic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,12 @@ include("../src/SpinMonteCarlo.jl")
 using SpinMonteCarlo
 
 const SEED = 19937
-const MCS = 20000
+const MCS = 10000
 const Therm = MCS
 const alpha = 0.01
 
 @testset begin
     for filename in ("lattice.jl",
-                     "anisotropic.jl",
                      "classical.jl",
                      "quantum.jl",
                     )
