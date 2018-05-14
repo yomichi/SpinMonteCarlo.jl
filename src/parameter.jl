305937bb825f983920ef4b2f69cbe049ac054ec9
Yuichi Motoyama <yomichi@tsg.jp>
2018-05-14 19:18:29 +0900

    speed up

--- src/parameter.jl
+++ src/parameter.jl
@@ -2,38 +2,31 @@ const Parameter = Dict{String, Any}
 
 function convert_parameter(model::Union{Ising, Potts, Clock, XY}, param::Parameter)
     T = Float64(param["T"])
-    J = get(param, "J", 1.0)
-    if isa(J, Real)
-        Js = Float64(J).*ones(numbondtypes(model))
-    else
-        Js = Vector{Float64}(J)
-    end
+    nbt = numbondtypes(model)
+
+    J = get(param, "J", 1.0) :: Union{Float64, AbstractArray{Float64}}
+    Js = zeros(nbt)
+    Js .= J
     return T, Js
 end
 
 function convert_parameter(model::QuantumXXZ, param::Parameter)
     T = Float64(param["T"])
 
+    nbt = numbondtypes(model)
+    nst = numsitetypes(model)
+
     Jz = get(param, "Jz", 1.0)
-    if isa(Jz, Real)
-        Jzs = Float64(Jz).*ones(numbondtypes(model))
-    else
-        Jzs = Vector{Float64}(Jz)
-    end
+    Jzs = zeros(nbt)
+    Jzs .= Jz
 
     Jxy = get(param, "Jxy", 1.0)
-    if isa(Jxy, Real)
-        Jxys = Float64(Jxy).*ones(numbondtypes(model))
-    else
-        Jxys = Vector{Float64}(Jxy)
-    end
+    Jxys = zeros(nbt)
+    Jxys .= Jxy
 
     G = get(param, "Gamma", 0.0)
-    if isa(G, Real)
-        Gs = Float64(G).*ones(numsitetypes(model))
-    else
-        Gs = Vector{Float64}(G)
-    end
+    Gs = zeros(nbt)
+    Gs .= G
 
     return T, Jzs, Jxys, Gs
 end
