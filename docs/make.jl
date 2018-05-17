e25f1b97af6866c8f5e826d68ccef956af4fe3ad
Yuichi Motoyama <yomichi@tsg.jp>
2018-05-17 12:37:10 +0900

    deproy

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,21 @@
 using Documenter
 using SpinMonteCarlo
 
-makedocs(
-         format=:html,
-         sitename="SpinMonteCarlo.jl",
-         modules=[SpinMonteCarlo],
-         pages = [
-                  "Home" => "index.md",
-                  "Develop your program" => "develop.md",
-                  "Library" => Any[
-                    "Public" => "lib/public.md",
-                    "Interanals" => "lib/internals.md",
-                   ]
-                 ]
+makedocs( format=:html,
+          sitename="SpinMonteCarlo.jl",
+          modules=[SpinMonteCarlo],
+          pages = [ "Home" => "index.md",
+                    "Develop your program" => "develop.md",
+                    "Library" => Any[
+                      "Public" => "lib/public.md",
+                      "Interanals" => "lib/internals.md",
+                     ]
+                  ]
         )
+
+deploydocs( repo = "github.com/yomichi/SpinMonteCarlo.jl.git",
+            julia="0.6",
+            target="build",
+            deps = nothing,
+            make = nothing,
+          )
